2d5eae16627f37a8d95bcd279a4814c27467c250
Sedinam <sedinam436@gmail.com>
2022-10-14 16:50:15 -0200

    created the screens folder

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import  { useFonts } from 'expo-font';
 
-export default function App() {
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator ();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
+const App = () => {
+  const [loaded] =useFonts({
+    
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Details" component={Details}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
