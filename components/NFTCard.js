6d04452a559fe663f287b4f4c0ad8a023da8b7e5
Sedinam <sedinam436@gmail.com>
2022-10-15 14:29:40 -0200

    added components folder and files

---
+++ components/NFTCard.js
@@ -0,0 +1,22 @@
+import { View, Image, Text } from 'react-native';
+// import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+const NFTCard = ({data}) => {
+  const navigation = useNavigation();
+  return (
+    <View style={{
+      backgroundColor:COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom:SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+      <Text>NFTCard</Text>
+    </View>
+  )
+}
+
+export default NFTCard
