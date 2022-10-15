3cbff0c64afc54c490c34a361705ac55f1d89b0e
Sedinam <sedinam436@gmail.com>
2022-10-15 13:47:58 -0200

    added Button file to the components folder

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -4,6 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants';
 
+import { CircleButton, RectButton } from './Button';
+
 const NFTCard = ({data}) => {
   const navigation = useNavigation();
   return (
@@ -14,7 +16,20 @@ const NFTCard = ({data}) => {
       margin: SIZES.base,
       ...SHADOWS.dark
     }}>
-      <Text>NFTCard</Text>
+      <View style={{width:"100%", height:250}}>
+          <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width:"100%",
+            height:"100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+          />
+
+          <CircleButton imgUrl={assets.heart} right={10} top={10}/>
+      </View>
     </View>
   )
 }
